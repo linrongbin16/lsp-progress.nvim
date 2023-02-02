be461c82c1fd44d7f062b73582462f546b228df4
Rongbin Lin <rlin@flexport.com>
2023-02-02 23:56:13 +0800

    fix CLIENTS methods

--- lua/lsp-progress.lua
+++ lua/lsp-progress.lua
@@ -273,21 +273,21 @@ end
 -- {
 -- CLIENTS
 
-function CLIENTS:hasClient(client_id)
-    return self[client_id] ~= nil
+function hasClient(client_id)
+    return CLIENTS[client_id] ~= nil
 end
 
-function CLIENTS:getClient(client_id)
-    return self[client_id]
+function getClient(client_id)
+    return CLIENTS[client_id]
 end
 
-function CLIENTS:removeClient(client_id)
-    self[client_id] = nil
+function removeClient(client_id)
+    CLIENTS[client_id] = nil
 end
 
-function CLIENTS:registerClient(client_id, client_name)
-    if not self:hasClient(client_id) then
-        self[client_id] = new_client(client_id, client_name)
+function registerClient(client_id, client_name)
+    if not hasClient(client_id) then
+        CLIENTS[client_id] = new_client(client_id, client_name)
         LOGGER:debug("Register client (client_id:" .. client_id .. ", client_name:" .. client_name .. ") in CLIENTS")
     end
 end
@@ -301,11 +301,11 @@ local function spinStart(client_id, token)
         spinStart(client_id, token)
     end
 
-    if not CLIENTS:hasClient(client_id) then
+    if not hasClient(client_id) then
         LOGGER:debug("Series not found: client_id:" .. client_id .. " not exist (token:" .. token .. "), stop spin")
         return
     end
-    local client = CLIENTS:getClient(client_id)
+    local client = getClient(client_id)
     if not client:hasSeries(token) then
         LOGGER:debug(
             "Series not found: token:" .. token .. " not exist in CLIENTS[" .. client_id .. "].serieses, stop spin"
@@ -321,7 +321,7 @@ local function spinStart(client_id, token)
     -- if series done, remove this series from data in decay time
     if series.done then
         local function remove_series_later()
-            if not CLIENTS:hasClient(client_id) then
+            if not hasClient(client_id) then
                 LOGGER:debug(
                     "Series not found: client_id:"
                         .. client_id
@@ -331,7 +331,7 @@ local function spinStart(client_id, token)
                 )
                 return
             end
-            local client = CLIENTS:getClient(client_id)
+            local client = getClient(client_id)
             if not client:hasSeries(token) then
                 LOGGER:debug(
                     "Series not found: token:"
@@ -346,7 +346,7 @@ local function spinStart(client_id, token)
             LOGGER:debug("Series removed (client_id:" .. client_id .. ",token:" .. token .. ")")
             if client:empty() then
                 -- if client is empty, also remove it from CLIENTS
-                CLIENTS:removeClient(client_id)
+                removeClient(client_id)
             end
             emitEvent() -- notify user
         end
@@ -362,12 +362,12 @@ local function progress_handler(err, msg, ctx)
     local client_name = the_neovim_client and the_neovim_client.name or "unknown"
 
     -- register client id if not exist
-    CLIENTS:registerClient(client_id, client_name)
+    registerClient(client_id, client_name)
 
     local value = msg.value
     local token = msg.token
 
-    local client = CLIENTS:getClient(client_id)
+    local client = getClient(client_id)
     if value.kind == "begin" then
         -- add task
         local series = new_series(value.title, value.message, value.percentage)
@@ -408,7 +408,7 @@ local function progress()
     for client_id, client_data in pairs(CLIENTS) do
         if vim.lsp.client_is_stopped(client_id) then
             -- if this client is stopped, remove it from CLIENTS
-            CLIENTS:removeClient(client_id)
+            removeClient(client_id)
         else
             local deduped_serieses = {}
             for token, series in pairs(client_data.serieses) do
