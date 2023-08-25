ff083cb5c7a9293be8bffe2824771bd1889ff790
linrongbin16 <linrongbin16@outlook.com>
2023-08-25 09:41:02 +0800

    refactor: typecheck

--- lua/lsp-progress/logger.lua
+++ lua/lsp-progress/logger.lua
@@ -1,18 +1,37 @@
+--- @type string
+local PATH_SEPARATOR = (vim.fn.has("win32") > 0 or vim.fn.has("win64") > 0)
+        and "\\"
+    or "/"
+
+--- @type table<string, integer>
+local LogLevels = {
+    TRACE = 0,
+    DEBUG = 1,
+    INFO = 2,
+    WARN = 3,
+    ERROR = 4,
+    OFF = 5,
+}
+
 --- @type table<string, string>
 local EchoHl = {
-    ["ERROR"] = "ErrorMsg",
-    ["WARN"] = "ErrorMsg",
-    ["INFO"] = "None",
-    ["DEBUG"] = "Comment",
+    ERROR = "ErrorMsg",
+    WARN = "WarningMsg",
+    INFO = "None",
+    DEBUG = "Comment",
 }
---- @type string
-local LogLevel = "INFO"
---- @type boolean|nil
-local UseConsole = nil
---- @type boolean|nil
-local UseFile = nil
---- @type string|nil
-local FileName = nil
+
+--- @type table<string, string|boolean|nil>
+local Configs = {
+    --- @type string
+    level = "INFO",
+    --- @type boolean?
+    use_console = nil,
+    --- @type boolean?
+    use_file = nil,
+    --- @type string?
+    file_name = nil,
+}
 
 --- @param debug boolean
 --- @param console_log boolean
@@ -21,46 +40,41 @@ local FileName = nil
 --- @return nil
 local function setup(debug, console_log, file_log, file_log_name)
     if debug then
-        LogLevel = "DEBUG"
+        Configs.level = "DEBUG"
     end
-    UseConsole = console_log
-    UseFile = file_log
+    Configs.use_console = console_log
+    Configs.use_file = file_log
     -- For Windows: $env:USERPROFILE\AppData\Local\nvim-data\lsp-progress.log
     -- For *NIX: ~/.local/share/nvim/lsp-progress.log
-    FileName = string.format("%s/%s", vim.fn.stdpath("data"), file_log_name)
+    Configs.file_name = string.format(
+        "%s%s%s",
+        vim.fn.stdpath("data"),
+        PATH_SEPARATOR,
+        file_log_name
+    )
 end
 
---- @param level string
+--- @param level "ERROR"|"WARN"|"INFO"|"DEBUG"
 --- @param msg string
 --- @return nil
 local function log(level, msg)
-    if vim.log.levels[level] < vim.log.levels[LogLevel] then
+    if LogLevels[level] < LogLevels[Configs.level] then
         return
     end
 
-    local msg_lines = vim.split(msg, "\n")
-    if UseConsole then
+    local msg_lines = vim.fn.split(msg, "\n")
+    if Configs.use_console then
         local msg_chunks = {}
-        for _, mline in ipairs(msg_lines) do
+        for _, line in ipairs(msg_lines) do
             table.insert(msg_chunks, {
-                string.format("[lsp-progress] %s\n", mline),
+                string.format("[lsp-progress] %s\n", line),
                 EchoHl[level],
             })
         end
         vim.api.nvim_echo(msg_chunks, true, {})
-        -- vim.cmd("echohl " .. EchoHl[level])
-        -- for _, line in ipairs(msg_lines) do
-        --     vim.cmd(
-        --         string.format(
-        --             'echom "%s"',
-        --             vim.fn.escape(string.format("[lsp-progress] %s", line), '"')
-        --         )
-        --     )
-        -- end
-        -- vim.cmd("echohl None")
     end
-    if UseFile then
-        local fp = io.open(FileName --[[@as string]], "a")
+    if Configs.use_file then
+        local fp = io.open(Configs.file_name --[[@as string]], "a")
         if fp then
             for _, line in ipairs(msg_lines) do
                 fp:write(
