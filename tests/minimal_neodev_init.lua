d647a46f8f5c44c7c98410811e22595beeeb2836
linrongbin16 <linrongbin16@outlook.com>
2023-10-02 20:58:29 +0800

    feat: use 'pcall' on configured 'format' function (#66)
    * feat: use pcall on configured 'format'
    
    * test case

--- tests/minimal_neodev_init.lua
+++ tests/minimal_neodev_init.lua
@@ -83,6 +83,24 @@ local plugins = {
                         function()
                             return require("lsp-progress").progress({
                                 max_size = 80,
+                                format = function(messages)
+                                    local active_clients =
+                                        vim.lsp.get_active_clients()
+                                    if #messages > 0 then
+                                        return table.concat(messages, " ")
+                                    end
+                                    local client_names = {}
+                                    for _, client in ipairs(active_clients) do
+                                        if client and client.name ~= "" then
+                                            table.insert(
+                                                client_names,
+                                                1,
+                                                client.name
+                                            )
+                                        end
+                                    end
+                                    return table.concat(client_names, "  ")
+                                end,
                             })
                         end,
                         icon = { "", align = "right" },
