bcc5529eddc1f03cd6b3573b9692fe74a68d3b00
linrongbin16 <linrongbin16@outlook.com>
2023-10-11 16:45:08 +0800

    test(client): add test cases

--- test/client_spec.lua
+++ test/client_spec.lua
@@ -24,8 +24,22 @@ describe("client", function()
 
     client.setup(client_format, SPINNER)
 
+    local series = require("lsp-progress.series")
+
+    local function series_formatter(title, message, percentage, done)
+        return string.format(
+            "%s, %s, %s, %s",
+            vim.inspect(title),
+            vim.inspect(message),
+            vim.inspect(percentage),
+            vim.inspect(done)
+        )
+    end
+
+    series.setup(series_formatter)
+
     describe("[_get_dedup_key]", function()
-        it("normal strings", function()
+        it("make dedup keys", function()
             assert_eq(
                 client._get_dedup_key("title", "message"),
                 "title-message"
@@ -42,131 +56,79 @@ describe("client", function()
             assert_eq(cli.client_name, "luals")
             assert_eq(cli.spin_index, 0)
             assert_eq(client_format("luals", SPINNER[1], {}), cli._format_cache)
+            assert_eq(client_format("luals", SPINNER[1], {}), cli:format())
             assert_false(cli:has_series("asdf"))
         end)
         it("_format", function()
-            local ss = series.Series:new("title", "message", 10)
-            assert_eq(type(ss), "table")
-            assert_eq(ss.title, "title")
-            assert_eq(ss.message, "message")
-            assert_eq(ss.percentage, 10)
+            local cli = client.Client:new(2, "clangd")
+            assert_eq(type(cli), "table")
+            assert_eq(cli.client_id, 2)
+            assert_eq(cli.client_name, "clangd")
+            assert_eq(cli.spin_index, 0)
 
             assert_eq(
-                series_formatter(ss.title, ss.message, ss.percentage, ss.done),
-                ss:_format()
+                client_format("clangd", SPINNER[1], {}),
+                cli._format_cache
             )
+            assert_eq(client_format("clangd", SPINNER[1], {}), cli:format())
         end)
-        it("update", function()
+        it("add series", function()
+            local cli = client.Client:new(2, "clangd")
+            assert_eq(type(cli), "table")
+            assert_eq(cli.client_id, 2)
+            assert_eq(cli.client_name, "clangd")
+            assert_eq(cli.spin_index, 0)
+
+            assert_eq(client_format("clangd", SPINNER[1], {}), cli:format())
+            assert_eq(
+                client_format("clangd", SPINNER[1], {}),
+                cli:format_result()
+            )
+
             local ss = series.Series:new("title", "message", 10)
-            assert_eq(type(ss), "table")
-            assert_eq(ss.title, "title")
-            assert_eq(ss.message, "message")
-            assert_eq(ss.percentage, 10)
-            assert_eq(
-                series_formatter(ss.title, ss.message, ss.percentage, ss.done),
-                ss:_format()
-            )
+            cli:add_series("token1", ss)
 
-            ss:update("message2", 20)
-            assert_eq(ss.message, "message2")
-            assert_eq(ss.percentage, 20)
-            assert_eq(
-                series_formatter(ss.title, "message2", 20, ss.done),
-                ss:_format()
-            )
+            assert_true(cli:has_series("token1"))
+            assert_false(cli:empty())
 
-            ss:update("", 30)
-            assert_eq(ss.message, "message2")
-            assert_eq(ss.percentage, 30)
-            assert_eq(
-                series_formatter(ss.title, "message2", 30, ss.done),
-                ss:_format()
-            )
+            local ss2 = cli:get_series("token1")
+            assert_eq(type(ss2), "table")
+            assert_eq(ss2.title, ss.title)
+            assert_eq(ss2.message, ss.message)
+            assert_eq(ss2.percentage, ss.percentage)
+            assert_eq(ss2.done, ss.done)
 
-            ss:update(nil, 40)
-            assert_eq(ss.message, "message2")
-            assert_eq(ss.percentage, 40)
-            assert_eq(
-                series_formatter(ss.title, "message2", 40, ss.done),
-                ss:_format()
-            )
-
-            ss:update("message5", 50)
-            assert_eq(ss.message, "message5")
-            assert_eq(ss.percentage, 50)
-            assert_eq(
-                series_formatter(ss.title, "message5", 50, ss.done),
-                ss:_format()
-            )
+            assert_false(cli:has_series("token2"))
+            assert_true(cli:get_series("token2") == nil)
         end)
-        it("finish", function()
+        it("remove series", function()
+            local cli = client.Client:new(2, "clangd")
+            assert_eq(type(cli), "table")
+            assert_eq(cli.client_id, 2)
+            assert_eq(cli.client_name, "clangd")
+            assert_eq(cli.spin_index, 0)
+
+            assert_eq(client_format("clangd", SPINNER[1], {}), cli:format())
+            assert_eq(
+                client_format("clangd", SPINNER[1], {}),
+                cli:format_result()
+            )
+
             local ss = series.Series:new("title", "message", 10)
-            assert_eq(type(ss), "table")
-            assert_eq(ss.title, "title")
-            assert_eq(ss.message, "message")
-            assert_eq(ss.percentage, 10)
-            assert_eq(
-                series_formatter(ss.title, ss.message, ss.percentage, ss.done),
-                ss:_format()
-            )
+            cli:add_series("token1", ss)
 
-            ss:finish("message2")
-            assert_eq(ss.message, "message2")
-            assert_eq(ss.percentage, 100)
-            assert_eq(ss.done, true)
-            assert_eq(
-                series_formatter(ss.title, "message2", 100, true),
-                ss:_format()
-            )
+            assert_true(cli:has_series("token1"))
+            assert_false(cli:empty())
 
-            ss:finish("")
-            assert_eq(ss.message, "message2")
-            assert_eq(ss.percentage, 100)
-            assert_eq(ss.done, true)
-            assert_eq(
-                series_formatter(ss.title, "message2", 100, ss.done),
-                ss:_format()
-            )
+            local ss2 = cli:get_series("token1")
+            assert_eq(type(ss2), "table")
+            assert_eq(ss2.title, ss.title)
+            assert_eq(ss2.message, ss.message)
+            assert_eq(ss2.percentage, ss.percentage)
+            assert_eq(ss2.done, ss.done)
 
-            ss:finish(nil)
-            assert_eq(ss.message, "message2")
-            assert_eq(ss.percentage, 100)
-            assert_eq(ss.done, true)
-            assert_eq(
-                series_formatter(ss.title, "message2", 100, ss.done),
-                ss:_format()
-            )
-
-            ss:finish("message5")
-            assert_eq(ss.message, "message5")
-            assert_eq(ss.percentage, 100)
-            assert_eq(ss.done, true)
-            assert_eq(
-                series_formatter(ss.title, "message5", 100, ss.done),
-                ss:_format()
-            )
-            assert_eq(
-                series_formatter(ss.title, "message5", 100, ss.done),
-                ss:format_result()
-            )
-        end)
-    end)
-    describe("[_choose_updated_message]", function()
-        it("choose non-empty", function()
-            assert_eq(series._choose_updated_message(nil, "asdf"), "asdf")
-            assert_eq(series._choose_updated_message("", "asdf"), "asdf")
-            assert_eq(
-                series._choose_updated_message("asdfasdf", "asdf"),
-                "asdf"
-            )
-            assert_eq(
-                series._choose_updated_message("asdfasdf", ""),
-                "asdfasdf"
-            )
-            assert_eq(
-                series._choose_updated_message("asdfasdf", nil),
-                "asdfasdf"
-            )
+            cli:remove_series("token1")
+            assert_false(cli:has_series("token1"))
         end)
     end)
 end)
