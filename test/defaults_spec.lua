ac842eb53d5ba38d539400f249eae4612b43f671
linrongbin16 <linrongbin16@outlook.com>
2023-10-11 22:29:50 +0800

    test(client_manager): add tests

--- test/defaults_spec.lua
+++ test/defaults_spec.lua
@@ -18,6 +18,29 @@ describe("defaults", function()
             assert_eq(df.spin_update_time, 200)
             assert_eq(df.decay, 700)
             assert_eq(df.event, "LspProgressStatusUpdated")
+            assert_eq(df.event_update_time_limit, 100)
+            assert_eq(df.max_size, -1)
+            assert_eq(df.regular_internal_update_time, 500)
+            assert_eq(type(df.series_format), "function")
+            assert_eq(
+                type(df.series_format("title", "message", 10, true)),
+                "string"
+            )
+            assert_true(
+                string.len(df.series_format("title", "message", 10, true)) > 0
+            )
+            assert_eq(type(df.client_format), "function")
+            assert_eq(
+                type(df.client_format("luals", "x", { "a", "b", "c" })),
+                "string"
+            )
+            assert_true(
+                string.len(df.client_format("luals", "x", { "a", "b", "c" }))
+                    > 0
+            )
+            assert_eq(type(df.format), "function")
+            assert_eq(type(df.format({ "a", "b", "c" })), "string")
+            assert_true(string.len(df.format({ "a", "b", "c" })) > 0)
         end)
     end)
 end)
